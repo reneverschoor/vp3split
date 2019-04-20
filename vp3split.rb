6559830239104664ee944d840faede4759a50b6e
Rene <reneverschoor@gmail.com>
2019-04-20 22:22:21 +0200

    Extract first color and write to file

--- vp3split.rb
+++ vp3split.rb
@@ -29,137 +29,163 @@ abort("Invalid unknown_c") unless unknown_c == "c"
 
 design_block_count = file.read(2).unpack("n").first
 puts "Designs in this file: #{design_block_count}"
+abort("I can only handle files with one design") unless design_block_count == 1
 
 # Design block
-design_block_count.times do
+tag = file.read(3)
+abort("Invalid DesignBlock tag") unless tag == "\x00\x03\x00"
+
+cursor_bytes_to_end_of_design = file.tell
+bytes_to_end_of_design = file.read(4).unpack("N").first  # !!! needs to be modified
+#puts "Cursor = #{cursor_bytes_to_end_of_design}"
+#puts "Bytes to end of design = #{bytes_to_end_of_design}"
+
+design_center_x = file.read(4).unpack("N")
+design_center_y = file.read(4).unpack("N")
+
+unknown_000_1 = file.read(3)
+abort("Invalid unknown_000_1 element") unless unknown_000_1 == "\x00\x00\x00"
+
+min_half_width = file.read(4).unpack("N")
+plus_half_width = file.read(4).unpack("N")
+min_half_heigth = file.read(4).unpack("N")
+plus_half_heigth = file.read(4).unpack("N")
+width = file.read(4).unpack("N")
+height = file.read(4).unpack("N")
+
+design_notes_string_length = file.read(2).unpack("n").first
+design_notes_string = file.read(design_notes_string_length)
+
+unknown_dd = file.read(2)
+abort("Invalid unknown_dd element") unless unknown_dd == "dd"
+unknown_101 = file.read(16)
+abort("Invalid unknown_101 element") unless unknown_101 == "\x00\x00\x10\x00" + "\x00\x00\x00\x00" + "\x00\x00\x00\x00" + "\x00\x00\x10\x00"
+unknown_xxpp = file.read(4)
+abort("Invalid unknown_xxpp element") unless unknown_xxpp == "xxPP"
+unknown_10 = file.read(2)
+abort("Invalid unknown_10 element") unless unknown_10 == "\x01\x00"
+
+production_string_length = file.read(2).unpack("n").first
+production_string = file.read(production_string_length)
+
+color_block_count = file.read(2).unpack("n").first
+puts "This design has #{color_block_count} colors"
+
+######
+# cursor_bytes_to_end_of_design
+#  4 (bytes_to_end_of_design)
+#
+#  8 (design_center_x/y)
+#  3 (unknown)
+# 24 (width/height)
+#  2 (design_notes_string_length)
+# design_notes_string_length
+# 24 (unknown)
+#  2 (production_string_length)
+# production_string_length
+#  2 (color_block_count)
+# =========
+# 65 + design_notes_string_length + production_string_length
+#puts "Expected cursor 1st colorblock = #{cursor_bytes_to_end_of_design + 65 + design_notes_string_length + production_string_length}"
+# Color blocks
+
+color_block = []
+color_block_count.times do |color_nr|
+
+  cursor_color_block = file.tell
+  color_block[color_nr] = {:cursor => cursor_color_block}
+
+  puts
+  puts "Color \##{color_nr + 1}"
 
   tag = file.read(3)
-  abort("Invalid DesignBlock tag") unless tag == "\x00\x03\x00"
+  abort("Invalid ColorBlock tag") unless tag == "\x00\x05\x00"
 
-  cursor_bytes_to_end_of_design = file.tell
-  bytes_to_end_of_design = file.read(4).unpack("N").first  # !!! needs to be modified
-  puts
-  puts "Cursor = #{cursor_bytes_to_end_of_design}"
-  puts "Bytes to end of design = #{bytes_to_end_of_design}"
+  bytes_to_next_color_block = file.read(4).unpack("N").first
+  color_block[color_nr][:blocksize] = bytes_to_next_color_block + 7  # 7 = tag + bytes_to_next_color_block
+  color_block_data = file.read(bytes_to_next_color_block)
 
-  design_center_x = file.read(4).unpack("N")
-  design_center_y = file.read(4).unpack("N")
+  # cursor + tag(3) + blocksize(4) + blocksize = next cursor
 
-  unknown_000_1 = file.read(3)
-  abort("Invalid unknown_000_1 element") unless unknown_000_1 == "\x00\x00\x00"
+  color_entries = color_block_data[8].unpack("C").first
+  abort("Thread colors is #{color_entries} instead of 1") unless color_entries == 1
 
-  min_half_width = file.read(4).unpack("N")
-  plus_half_width = file.read(4).unpack("N")
-  min_half_heigth = file.read(4).unpack("N")
-  plus_half_heigth = file.read(4).unpack("N")
-  width = file.read(4).unpack("N")
-  height = file.read(4).unpack("N")
-
-  design_notes_string_length = file.read(2).unpack("n").first
-  design_notes_string = file.read(design_notes_string_length)
-
-  unknown_dd = file.read(2)
-  abort("Invalid unknown_dd element") unless unknown_dd == "dd"
-  unknown_101 = file.read(16)
-  abort("Invalid unknown_101 element") unless unknown_101 == "\x00\x00\x10\x00" + "\x00\x00\x00\x00" + "\x00\x00\x00\x00" + "\x00\x00\x10\x00"
-  unknown_xxpp = file.read(4)
-  abort("Invalid unknown_xxpp element") unless unknown_xxpp == "xxPP"
-  unknown_10 = file.read(2)
-  abort("Invalid unknown_10 element") unless unknown_10 == "\x01\x00"
-
-  production_string_length = file.read(2).unpack("n").first
-  production_string = file.read(production_string_length)
-
-  color_block_count = file.read(2).unpack("n").first
-  puts "This design has #{color_block_count} colors"
-
-  ######
-  # cursor_bytes_to_end_of_design
-  #  4 (bytes_to_end_of_design)
-  #
-  #  8 (design_center_x/y)
-  #  3 (unknown)
-  # 24 (width/height)
-  #  2 (design_notes_string_length)
-  # design_notes_string_length
-  # 24 (unknown)
-  #  2 (production_string_length)
-  # production_string_length
-  #  2 (color_block_count)
-  # =========
-  # 65 + design_notes_string_length + production_string_length
-  #puts "Expected cursor 1st colorblock = #{cursor_bytes_to_end_of_design + 65 + design_notes_string_length + production_string_length}"
-  # Color blocks
-
-  color_block = []
-  color_block_count.times do |color_nr|
-
-    cursor_color_block = file.tell
-    color_block[color_nr] = {:cursor => cursor_color_block}
-
-    puts
-    puts "Color \##{color_nr + 1}"
-
-    tag = file.read(3)
-    abort("Invalid ColorBlock tag") unless tag == "\x00\x05\x00"
-
-    bytes_to_next_color_block = file.read(4).unpack("N").first
-    color_block[color_nr][:blocksize] = bytes_to_next_color_block + 7  # 7 = tag + bytes_to_next_color_block
-    color_block_data = file.read(bytes_to_next_color_block)
-
-    # cursor + tag(3) + blocksize(4) + blocksize = next cursor
-
-    color_entries = color_block_data[8].unpack("C").first
-    abort("Thread colors is #{color_entries} instead of 1") unless color_entries == 1
-
-    print "Material: "
-    case material = color_block_data[16].unpack("C").first
-    when 3
-      puts "Metallic"
-    when 4
-      puts "Cotton"
-    when 5
-      puts "Rayon"
-    when 6
-      puts "Polyester"
-    when 10
-      puts "Silk"
-    else
-      puts "Unknown material"
-    end
-
-    weight = color_block_data[17].unpack("C").first
-    puts "Weight: #{weight}"
-
-    catalog_length = color_block_data[18..19].unpack("n").first
-    catalog = color_block_data[20, catalog_length]
-    puts "Catalog: #{catalog}"
-
-    offset = 20 + catalog_length
-    description_length = color_block_data[offset, 2].unpack("n").first
-    offset += 2
-    description = color_block_data[offset, description_length]
-    puts "Description: #{description}"
-
-    offset += description_length
-    brand_length = color_block_data[offset, 2].unpack("n").first
-    offset += 2
-    brand = color_block_data[offset, brand_length]
-    puts "Brand: #{brand}"
-
-    #exit
+  print "Material: "
+  case material = color_block_data[16].unpack("C").first
+  when 3
+    puts "Metallic"
+  when 4
+    puts "Cotton"
+  when 5
+    puts "Rayon"
+  when 6
+    puts "Polyester"
+  when 10
+    puts "Silk"
+  else
+    puts "Unknown material"
   end
 
-  puts
-  p color_block
+  weight = color_block_data[17].unpack("C").first
+  puts "Weight: #{weight}"
 
-  puts "bytes_to_end_of_design = #{bytes_to_end_of_design}"
-  puts "design block size = #{65 + design_notes_string_length + production_string_length}"
+  catalog_length = color_block_data[18..19].unpack("n").first
+  catalog = color_block_data[20, catalog_length]
+  puts "Catalog: #{catalog}"
 
-  total = 65 + design_notes_string_length + production_string_length
-  color_block.each do |color|
-    total += color[:blocksize]
-  end
-  puts "total = #{total}"
+  offset = 20 + catalog_length
+  description_length = color_block_data[offset, 2].unpack("n").first
+  offset += 2
+  description = color_block_data[offset, description_length]
+  puts "Description: #{description}"
 
+  offset += description_length
+  brand_length = color_block_data[offset, 2].unpack("n").first
+  offset += 2
+  brand = color_block_data[offset, brand_length]
+  puts "Brand: #{brand}"
+
+  #exit
 end
+
+#p color_block
+#puts "bytes_to_end_of_design = #{bytes_to_end_of_design}"
+total = 65 + design_notes_string_length + production_string_length
+color_block.each do |color|
+  total += color[:blocksize]
+end
+#puts "total = #{total}"
+abort("Size mismatch") unless total == bytes_to_end_of_design
+
+
+file.rewind
+out = File.open("out.vp3", "wb")
+
+# test: write colorblock #1
+
+# read/write cursor_bytes_to_end_of_design bytes
+preamble = file.read(cursor_bytes_to_end_of_design)
+out.write(preamble)
+# calculate new bytes_to_end_of_design bytes
+# = 65 + design_notes_string_length + production_string_length + color_block[x][:blocksize]
+p total = 65 + design_notes_string_length + production_string_length
+p color_block
+p total += color_block[0][:blocksize]
+out.write([total].pack("N"))
+
+old_bytes_to_end = file.read(4)  # skip reading old value
+design_block = file.read(63 + design_notes_string_length + production_string_length) # don't read last 2 bytes = #colors
+out.write(design_block)
+
+new_nr_colors = 1
+out.write([new_nr_colors].pack("n"))
+
+# Move to colorblock
+cursor = color_block[0][:cursor]
+blocksize = color_block[0][:blocksize]
+file.pos = cursor
+colorblock = file.read(blocksize)
+out.write(colorblock
+)
+file.close
+out.close
